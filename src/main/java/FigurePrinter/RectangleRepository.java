11b42d3ebd8beb3a42f31d1f483863bc8004941a
braianpezet <braianpezet@gmail.com>
2023-05-07 21:25:07 -0300

    Ejercicio JPA

---
+++ src/main/java/FigurePrinter/RectangleRepository.java
@@ -0,0 +1,7 @@
+package FigurePrinter;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RectangleRepository extends CrudRepository<Rectangle, Long> {
+}
---
+++ src/main/java/FigurePrinter/SquareRepository.java
@@ -0,0 +1,6 @@
+package FigurePrinter;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SquareRepository  extends CrudRepository<Square, Long> {
+}
